59043e52c59d4873cea92b30ec38c7915d058189
Matt <msopata59@gmail.com>
2017-04-21 16:55:27 +0000

    final commit

--- server/subscriber.js
+++ server/subscriber.js
@@ -13,8 +13,8 @@ server.listen(1884);
 
 var location = "";
 
-var train1Blocks = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-var train2Blocks = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+// var train1Blocks = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+// var train2Blocks = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
 var train1SprogSpeeds = [];
 var train2SprogSpeeds = []; 
@@ -27,7 +27,10 @@ var pressure = [];
 var altitude = [];
 
 io.on('connection', function (socket) {
+    // read for the messages being sent over MQTT on the localhost 1883
+
     socket.on('message', function (msg) {
+        // only do calculations and send message if the user is on the certain page
         location = msg;
     });
     socket.on('disconnect', function () { });
@@ -43,37 +46,55 @@ io.on('connection', function (socket) {
         var m = message.toString('ascii');
         var js = JSON.parse(m);
         
-        if (js['name'] == "train1Block" || js['name'] == "train2Block") {
+        if (js['name'] == "sprogPower" && js['value'] == "ON") {
+            // refresh graphs on start up
+            train1SprogSpeeds = [];
+            train2SprogSpeeds = [];
+            train1ActualSpeeds = [];
+            train2ActualSpeeds = [];
+        } else if (js['name'] == "train1Block" || js['name'] == "train2Block") {
+            // send trainID, block, distance in block to train layout
+            // set up functionality to send to dashboard as well
+
             var id = 0;
-            var value = parseInt(js['value']);
+            var value = parseFloat(js['value']);
+            var block = Math.floor(value / 100);
+            var dist = value - block * 100.0;
+            
             if (js['name'] == "train1Block") { 
                 id = 0;
-                train1Blocks[value - 1] += 1;
+                // train1Blocks[value - 1] += 1;
             } else if (js['name'] == "train2Block") {
                 id = 1;
-                train2Blocks[value - 1] += 1;
+                // train2Blocks[value - 1] += 1;
             }
 
             if (location == "train-layout") {
-                var obj = JSON.parse(JSON.stringify({ 'id' : "updateTrain", 'trainID' : id, 'block' : value }));
+                var obj = JSON.parse(JSON.stringify({ 'id' : "updateTrain", 'trainID' : id, 'block' : block, 'dist' : dist }));
                 socket.send(obj);
-            } else if (location == "dashboard") {
-                if (id == 0) {
-                    var obj = JSON.parse(JSON.stringify({ 'id' : "train1Blocks", 'value' : train1Blocks }));
-                    socket.send(obj);
-                } else if (id == 1) {
-                    var obj = JSON.parse(JSON.stringify({ 'id' : "train2Blocks", 'train' : id, 'block' : value }));
-                    socket.send(obj);
-                }
             }
+            // } else if (location == "dashboard") {
+            //     if (id == 0) {
+            //          
+            //         var obj = JSON.parse(JSON.stringify({ 'id' : "train1Blocks", 'value' : train1Blocks }));
+            //         socket.send(obj);
+            //     } else if (id == 1) {
+            //         var obj = JSON.parse(JSON.stringify({ 'id' : "train2Blocks", 'train' : id, 'block' : value }));
+            //         socket.send(obj);
+            //     }
+            // }
         } else if (js['name'] == "message") {
+            // turn alert messages on and off in the train layout
             if (location == "train-layout") {
                 var obj = JSON.parse(JSON.stringify({ 'id' : js['name'], 'value' : js['value']}));
                 socket.send(obj);
             }
         } else if (js['name'] == "train1ActualSpeed") {
-           // min: 0, max: 1
-           // min: 0, max 20 
+            // send the last 100 train 1 actual speeds to the dashboard
+            // we are using the beam breakers to calculate the velocity
+            // also, we are calculating the mean in here
+            // we are keeping track of all velocities that we gathered, but only displaying last 100
+
             var value = parseFloat(js['value']);
             train1ActualSpeeds.push(value);
             if (location == "dashboard") {
@@ -96,6 +117,11 @@ io.on('connection', function (socket) {
                 socket.send(obj);
             } 
         } else if (js['name'] == "train2ActualSpeed") {
+            // send the last 100 train 2 actual speeds to the dashboard
+            // we are using the beam breakers to calculate the velocity
+            // also, we are calculating the mean in here
+            // we are keeping track of all velocities that we gathered, but only displaying last 100
+
             var value = parseFloat(js['value']);
             train2ActualSpeeds.push(value);
             if (location == "dashboard") {
@@ -118,6 +144,10 @@ io.on('connection', function (socket) {
                 socket.send(obj);
             }
         } else if (js['name'] == "train1SprogSpeed") {
+            // send the last 100 train 1 sprog speeds to the dashboard
+            // the sprog speed is a float from 0.0 to 1.0 (IE not moving - max speed)
+            // we are keeping track of all sppeds that we gathered, but only displaying last 100
+
             var value = parseFloat(js['value']);
             train1SprogSpeeds.push(value);
             if (location == "dashboard") {
@@ -140,6 +170,10 @@ io.on('connection', function (socket) {
                 socket.send(obj);
             }
         } else if (js['name'] == "train2SprogSpeed") {
+            // send the last 100 train 1 sprog speeds to the dashboard
+            // the sprog speed is a float from 0.0 to 1.0 (IE not moving - max speed)
+            // we are keeping track of all sppeds that we gathered, but only displaying last 100
+
             var value = parseFloat(js['value']);
             train2SprogSpeeds.push(value);
             if (location == "dashboard") {
@@ -163,6 +197,9 @@ io.on('connection', function (socket) {
                 socket.send(obj);
             }
         } else if (js['name'] == "temperature") {
+            // send the last 10 calculation of the temperature to the multisensor view
+            // also calculating the mean
+
             var value = parseFloat(js['value']);
             temperature.push(value);
             if (location == "multisensor") {
@@ -186,6 +223,9 @@ io.on('connection', function (socket) {
                 socket.send(obj);
             } 
         } else if (js['name'] == "pressure") {
+            // send the last 10 calculation of the pressure to the multisensor view
+            // also calculating the mean
+
             var value = parseInt(js['value']);   
             pressure.push(value);
             if (location == "multisensor") {
@@ -209,6 +249,9 @@ io.on('connection', function (socket) {
                 socket.send(obj);
             }
         } else if (js['name'] == "altitude") {
+            // send the last 10 calculation of the altitude  to the multisensor view
+            // also calculating the mean
+
             var value = parseFloat(js['value']);
             altitude.push(value);
             if (location == "multisensor") {
